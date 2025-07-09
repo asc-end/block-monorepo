45680617ee2518075fbf729e501d078bf8890d4b
mgavillo <marie.gavillon@gmail.com>
2025-07-09 16:02:53 +0400

    fix vite packages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@blockit/ui': path.resolve(__dirname, '../../packages/ui'),
-      '@blockit/cross-ui-toolkit': path.resolve(__dirname, '../../packages/cross-ui-toolkit'),
-    },
-  },
+  resolve: {},
   optimizeDeps: {
-    include: ['@ui'], // optional
+    include: ['@blockit/ui', '@blockit/cross-ui-toolkit', '@blockit/shared'], // optional
   },
   server: {
     fs: {
